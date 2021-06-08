bce6cd01a956743930d6e59d9d74ccce03d0add8
BlockOG <alesiobaca@gmail.com>
2021-06-08 20:38:51 +0300

    Added encode.py For Encoding And Decoding Base 74 Numbers

---
+++ encoder.py
@@ -0,0 +1,25 @@
+key = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$%&+-.=?^{}"
+
+encode_tuple = tuple(list(key))
+
+decode_dict = {}
+for i in range(74):
+    decode_dict[key[i]] = i
+
+
+def decode_string(char_string):
+    result = 0
+    for char in char_string:
+        result *= 74
+        result += decode_dict[char]
+    return result
+
+def encode_string(num):
+    result = []
+    counter = 0
+    
+    while num > (74 ** counter):
+        result.append(key[num // (74 ** counter) % 74])
+        counter += 1
+    
+    return "".join(result[::-1])
